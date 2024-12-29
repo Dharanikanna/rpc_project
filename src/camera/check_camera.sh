544b3764c4caeb5033ce7d660f1deefa056174ed
Dharani Kanna K V <dharanikanna1999@gmail.com>
2024-12-30 00:04:14 +0530

    Camera issue fixed

---
+++ src/camera/check_camera.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Setup virtual display
+Xvfb :99 -screen 0 1920x1080x24 &
+export DISPLAY=:99
+
+# Check if /dev/video0 is free
+if lsof /dev/video0; then
+  echo "/dev/video0 is in use. Exiting."
+  exit 1
+fi
+
+# Run libcamera-hello
+libcamera-hello

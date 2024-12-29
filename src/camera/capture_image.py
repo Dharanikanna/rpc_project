00c47bde4f826337c84c8f099eba7458afe6cc69
Dharani Kanna K V <dharanikanna1999@gmail.com>
2024-12-30 00:53:04 +0530

    capture image,video demo

--- src/camera/capture_image.py
+++ src/camera/capture_image.py
@@ -1,7 +1,4 @@
 from picamera2 import Picamera2
 
-
 picam2 = Picamera2()
-picam2.start()
-picam2.capture_file("src/camera/camera_output/image.jpg")
-picam2.stop()
+picam2.start_and_capture_file("src/camera/camera_output/image.jpg")
--- src/camera/capture_video.py
+++ src/camera/capture_video.py
@@ -1,21 +1,4 @@
 from picamera2 import Picamera2
-import time
 
-# Initialize the camera
 picam2 = Picamera2()
-
-# Configure the camera for video recording (you can customize resolution)
-video_config = picam2.create_video_configuration(main={"size": (1920, 1080)})
-picam2.configure(video_config)
-
-# Create a video output file
-output_path = "src/camera/camera_output/video.mp4"
-
-# Start recording video
-picam2.start_recording(output_path)
-
-# Record for 5 seconds
-time.sleep(5)
-
-# Stop recording
-picam2.stop_recording()
+picam2.start_and_record_video("src/camera/camera_output/video.mp4", duration=5)
